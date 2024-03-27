44ea4d8771f2449f236ca22d8dd5c5dc02f834ff
Can Umurhan Öksüz <146195084+canumurhan@users.noreply.github.com>
2024-03-27 12:35:57 +0200

    Add files via upload

---
+++ ev.tahminleme.case.py
@@ -0,0 +1,232 @@
+#Ev Fiyat Tahmin Modeli
+#İş Problemi
+
+#Her bir eve ait özelliklerin ve fiyatlarının bulunduğu veriseti
+# kullnılarak, farklı tipteki evlerin fiyatlarına ilişkin bir makine
+# öğrenmesi projesi gerçekleştirilmek istenmektedir.
+
+
+#Görev 1: Keşifçi Veri Analizi
+import warnings
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier
+from sklearn.model_selection import GridSearchCV, cross_validate, RandomizedSearchCV, validation_curve
+
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+
+warnings.simplefilter(action='ignore', category=Warning)
+
+
+#dım 1: Train ve Test veri setlerini okutup birleştiriniz.
+#Birleştirdiğiniz veri üzerinden ilerleyiniz
+df_train=pd.read_csv('datasets/train.csv')
+df_test=pd.read_csv('datasets/test.csv')
+df=pd.concat([df_train,df_test])
+df.head()
+
+#Adım 2:Numerikvekategorikdeğişkenleriyakalayınız.
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+
+num_cols = [col for col in num_cols if col not in "Id"]
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+cat_cols
+num_cols
+cat_but_car
+#Adım 3: Gerekli düzenlemeleri yapınız. (Tip hatası olan değişkenler gibi)
+
+df.info()
+
+#Adım 4:  Numerik ve kategorik değişkenlerin veri içindeki dağılımını gözlemleyiniz.
+##################################
+# KATEGORİK DEĞİŞKENLERİN ANALİZİ
+##################################
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show()
+
+for col in cat_cols:
+    cat_summary(df, col)
+
+##################################
+# NUMERİK DEĞİŞKENLERİN ANALİZİ
+##################################
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show()
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+#Adım 5: Kategorik değişkenler ile hedef değişken incelemesini yapınız.
+##################################
+# NUMERİK DEĞİŞKENLERİN TARGET GÖRE ANALİZİ
+##################################
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+
+for col in num_cols:
+    target_summary_with_num(df, "SalePrice", col)
+
+
+
+##################################
+# KATEGORİK DEĞİŞKENLERİN TARGET GÖRE ANALİZİ
+##################################
+
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(categorical_col)
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean(),
+                        "Count": dataframe[categorical_col].value_counts(),
+                        "Ratio": 100 * dataframe[categorical_col].value_counts() / len(dataframe)}), end="\n\n\n")
+
+for col in cat_cols:
+    target_summary_with_cat(df, "SalePrice", col)
+
+
+
+#Adım 6: Aykırı gözlem var mı inceleyiniz.
+
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def replace_with_thresholds(dataframe, variable, q1=0.05, q3=0.95):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable, q1=0.05, q3=0.95)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+# Aykırı Değer Analizi ve Baskılama İşlemi
+for col in num_cols:
+    print(col, check_outlier(df, col))
+    if check_outlier(df, col):
+        replace_with_thresholds(df, col)
+
+#Adım 7: Eksik gözlem var mı inceleyiniz.
+df.isnull().sum()
+
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
+    print(missing_df, end="\n")
+    if na_name:
+        return na_columns
+
+na_columns = missing_values_table(df, na_name=True)
+
+def fill_missing_values(dataframe, na_columns):
+    """
+    DataFrame içindeki eksik değerleri sütunların medianı ile doldurur.
+
+    Parameters:
+    dataframe (DataFrame): Eksik değerlerin doldurulacağı DataFrame.
+    na_columns (list): Eksik değerleri içeren sütunların listesi.
+
+    Returns:
+    None
+    """
+    for column in na_columns:
+        median_value = dataframe[column].median()
+        dataframe[column].fillna(median_value, inplace=True)
+
+# Eksik değerleri doldurma işlemi
+fill_missing_values(df, na_columns)
+
+#
+
